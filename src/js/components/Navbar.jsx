c4295e20284ff0842ce06a33ada144d9d7415fb8
Arturo Villalta <alvillalt@gmail.com>
2025-05-17 10:17:06 +0000

    Terminamos proyecto

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
             <a className="navbar-brand" href="#">Star Bootstrap</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
